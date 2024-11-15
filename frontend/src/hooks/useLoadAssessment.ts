518903f6935fe7a634ff6d31ec17026b8e46373a
suzoo-dev <57764195+suzoo-dev@users.noreply.github.com>
2024-11-15 23:00:28 +0000

    created useLoadAssessment hook

---
+++ frontend/src/hooks/useLoadAssessment.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { Assessment } from "../types";
+
+const useLoadAssessment = () => {
+  const [assessment, setAssessment] = useState<Assessment>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadAssessment = async () => {
+      try {
+        const assessmentRes = await fetch(
+          "http://127.0.0.1:8080/api/assessment/LearnerAssessment/1.5.0"
+        );
+        if (!assessmentRes.ok) {
+          throw new Error("Failed to load assessment");
+        }
+        const data = await assessmentRes.json();
+        setAssessment(data);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("Unknown error");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadAssessment();
+  }, []);
+
+  return { assessment, loading, error };
+};
+
+export default useLoadAssessment;
