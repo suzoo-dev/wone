286f6f51486778f3e0fdb856c651cbc3021893b7
suzoo-dev <57764195+suzoo-dev@users.noreply.github.com>
2024-11-16 15:22:13 +0000

    added toast for response submit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/toaster";
 import "./App.css";
-import useLoadAssessment from "./hooks/useLoadAssessment";
-import { useResponseStore } from "./store/useResponseStore";
-import StepComponent from "./components/Step";
+import Assessment from "./components/Assessment";
 
-const App = () => {
-  const { assessment, loading, error } = useLoadAssessment();
-  const { answers, resetAnswers } = useResponseStore();
-
-  const handleSubmit = async () => {
-    console.log(answers);
-    try {
-      const response = await fetch("http://127.0.0.1:8080/api/response", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ responses: answers }),
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to submit response");
-      }
-
-      console.log("Response submitted successfully");
-      resetAnswers();
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        console.error(err.message);
-      } else {
-        console.error("Unknown error");
-      }
-    }
-  };
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>{error}</div>;
-  if (!assessment) return <div>No assessment found</div>;
-
-  /**
-   * TODO:
-   * - [x] Save responses
-   * - [ ] Break steps into groups based on id prefix
-   * - [ ] Load previous responses
-   * - [ ] Add step navigation
-   * - [ ] Add progress bar
-   */
-
+export default function App() {
   return (
     <>
-      {assessment
-        ? assessment.steps.map((step, index) => (
-            <StepComponent key={index} step={step} />
-          ))
-        : null}
-      <Button onClick={handleSubmit}>Submit</Button>
+      <Assessment />
+      <Toaster />
     </>
   );
-};
-
-export default App;
+}
---
+++ frontend/src/components/Assessment.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import useLoadAssessment from "../hooks/useLoadAssessment";
+import { useResponseStore } from "../store/useResponseStore";
+import StepComponent from "./Step";
+
+const Assessment = () => {
+  const { toast } = useToast();
+  const { assessment, loading, error } = useLoadAssessment();
+  const { answers, resetAnswers } = useResponseStore();
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/api/response", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ responses: answers }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to submit response");
+      }
+
+      toast({ description: "Response submitted successfully" });
+      resetAnswers();
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error(err.message);
+      } else {
+        console.error("Unknown error");
+      }
+    }
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+  if (!assessment) return <div>No assessment found</div>;
+
+  return (
+    <>
+      {assessment
+        ? assessment.steps.map((step, index) => (
+            <StepComponent key={index} step={step} />
+          ))
+        : null}
+      <Button onClick={handleSubmit}>Submit</Button>
+    </>
+  );
+};
+
+export default Assessment;
