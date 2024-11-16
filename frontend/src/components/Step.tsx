8d0e04aac26acf55a2d7c28919f34a672ef0c638
suzoo-dev <57764195+suzoo-dev@users.noreply.github.com>
2024-11-16 00:00:35 +0000

    swap logical renders for ternary

--- frontend/src/components/Step.tsx
+++ frontend/src/components/Step.tsx
@@ -9,10 +9,10 @@ const StepComponent: React.FC<StepComponentProps> = ({ step }) => {
   if (step.type === "ASSESSMENT_INFO_STEP") {
     return (
       <div>
-        {step.title && <h2>{step.title}</h2>}
-        {step.subtitle && (
+        {step.title ? <h2>{step.title}</h2> : null}
+        {step.subtitle ? (
           <div dangerouslySetInnerHTML={{ __html: step.subtitle }}></div>
-        )}
+        ) : null}
       </div>
     );
   }
@@ -24,9 +24,9 @@ const StepComponent: React.FC<StepComponentProps> = ({ step }) => {
     return (
       <div>
         <h2 dangerouslySetInnerHTML={{ __html: step.title }}></h2>
-        {step.subtitle && (
+        {step.subtitle ? (
           <div dangerouslySetInnerHTML={{ __html: step.subtitle }}></div>
-        )}
+        ) : null}
         <select
           required={input?.required}
           aria-label={input?.label || "Select an option"}
